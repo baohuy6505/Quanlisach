49a973588ef7d1713d3db28afa219ab893ee11a9
SoKhuyngthanh <nholanhathuy@gmail.com>
2024-12-21 21:42:48 +0700

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,17 +1,28 @@
-package quan_ly_sach;
+package quan_ly_tai_lieu;
 
 public class Book extends Document
 {
+    private String name_book;
     private String name_author;
     private int num_page;
     
     public Book() {}
-    public Book(String id, String name_nxb, int release_num, String name_author, int num_page)
+    public Book(String id, String name_nxb, int release_num, String name_book, String name_author, int num_page)
     {
         super(id, name_nxb, release_num);
+        this.name_book = name_book;
         this.name_author = name_author;
         this.num_page = num_page;
     }
+    public void setName_book(String name_book)
+    {
+        this.name_book = name_book;
+    }
+    public String getName_book()
+    {
+        return this.name_book;
+    }
+
     public void setName_author(String name_author)
     {
         this.name_author = name_author;
@@ -35,29 +46,11 @@ public class Book extends Document
         System.out.println("Book: - Id: " + getId());
         System.out.println("      - NXB: " + getName_nxb());
         System.out.println("      - So ban phat hanh:  " + getNum_page());
+        System.out.println("      - Ten sach:  " + getName_book());
         System.out.println("      - Tac gia: " + getName_author());
         System.out.println("      - So trang: " + getNum_page());
     }
 } 
 
-/*package quan_ly_sach;
 
-import javax.swing.JTextArea;
-
-public class Book extends Document 
-{
-    private String author;
-    private int numPage;
-
-    public Book(String id, String nameNXB, int releaseNum, String author, int numPage) {
-        super(id, nameNXB, releaseNum);
-        this.author = author;
-        this.numPage = numPage;
-    }
-
-    @Override
-    public void displayInfo(JTextArea textArea) {
-        textArea.append("Book ID: " + id + ", NXB: " + nameNXB + ", Release Number: " + releaseNum + ", Author: " + author + ", Number of Pages: " + numPage);
-    }
-} */
 
